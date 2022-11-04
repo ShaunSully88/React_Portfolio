36183ac33ed99d84f97611ee9543029fb89269f1
Shaun Sullivan <shaunsully88@gmail.com>
2022-11-04 10:38:17 -0400

    apps added to page. Carousel view added as well

--- src/components/Apps/index.js
+++ src/components/Apps/index.js
@@ -1,30 +1,76 @@
 import React from 'react';
 import Col from 'react-bootstrap/Col';
-import BooksAndBeans from '../../assets/project/0.jpg';
+import EmployeeTracker from '../../assets/project/1.jpg';
 import OpenFields from '../../assets/project/2.jpg';
 import Greenside from '../../assets/project/3.jpg';
 import VeganMart from '../../assets/project/4.png';
 
-function Apps( item ) {
-    
-    const { img, i, category, link } = item;
+function Apps () {
     return (
-        <>
-        <Col md={4}>
-        <div className="portfolio_container">
-            <div className="portfolio_container-info">
-                <a className="portfolio_container-name" href={link} target="_blank" rel="noreferrer">{img.name}</a>
-            </div>
+        <Carousel>
+        <Carousel.Item>
             <img
-                src={require(`../../assets/${category}/${i}.jpg`).default}
-                alt={img.name}
-                className="portfolio_container-image"
+                className="d-block w-100"
+                src={OpenFields}
+                alt="Open Fields"
             />
-        </div>
-        </Col>
-        </>
-    )
-}
+            <Carousel.Caption className="app1">
+                <p className="apps">Website that helps gardeners learn the basics of vegetable gardening and harvesting.</p> 
+                <p className="apps"> Languages: HTML, CSS, Third-party API </p>
+                <a className="githubrepo" href="https://github.com/ShaunSully88/Open-Fields" target="_blank" rel="noreferrer">GitHub Repo</a>
+                <a className="deployedprojects" href="https://shaunsully88.github.io/Open-Fields/" target="_blank" rel="noreferrer">Deployed Link</a>
+            </Carousel.Caption>
+        </Carousel.Item>
 
+
+
+        <Carousel.Item>
+            <img
+                className="d-block w-100"
+                src={EmployeeTracker}
+                alt="Employee Tracker"
+            />
+
+            <Carousel.Caption className="app1">
+                <p className="apps">Website that helps organize Employee database by different categories</p> 
+                <p className="apps"> Languages: SQL, Javascript, Node</p>
+                <a className="githubrepo" href="https://github.com/ShaunSully88/EmployeeTracker" target="_blank" rel="noreferrer">GitHub Repo</a>
+                <a className="deployedprojects" href="https://drive.google.com/file/d/1yLK-1TxzVoqAsq7Bv5mKL4EvmvP74gDP/view" target="_blank" rel="noreferrer">Deployed Link</a>
+            </Carousel.Caption>
+        </Carousel.Item>
+
+        <Carousel.Item>
+            <img
+                className="d-block w-100"
+                src={VeganMart}
+                alt="Vegan Mart"
+            />
+
+            <Carousel.Caption className="app1">
+                <p className="apps">E-commerce site for online grocery shopping</p> 
+                <p className="apps"> Languages: JavaScript, SQL, MongoDB, React </p>
+                <a className="githubrepo" href="https://github.com/ShaunSully88/VeganMart" target="_blank" rel="noreferrer">GitHub Repo</a>
+                <a className="deployedprojects" href="https://pure-spire-62048.herokuapp.com/" target="_blank" rel="noreferrer">Deployed Link</a>
+            </Carousel.Caption>
+        </Carousel.Item>
+
+        <Carousel.Item>
+            <img
+                className="d-block w-100"
+                src={Greenside}
+                alt="Greenside"
+            />
+
+            <Carousel.Caption className="app1">
+                <p className="apps">Website to search and filter for golf courses across Ontario</p> 
+                <p className="apps"> Languages: SQL, Javascsript, Express.js, React, MongoDB </p>
+                <a className="githubrepo" href="https://github.com/ShaunSully88/Greenside" target="_blank" rel="noreferrer">GitHub Repo</a>
+                
+            </Carousel.Caption>
+        </Carousel.Item>
+
+    </Carousel>
+    );
+}
 
 export default Apps;
