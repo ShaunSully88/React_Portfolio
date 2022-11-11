54aa4ec542146e02199b0282e7a3d25ddae28951
Shaun Sullivan <shaunsully88@gmail.com>
2022-11-11 14:50:03 -0500

    about me page altered. Adsded columns and set up z-index for navbar

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -11,30 +11,30 @@ function About() {
     return (
         <Container>
         <section className="my-1">
-            <Row lg="auto">
+            <Row lg="auto" class="row">
                 <Col>
                     <img src={coverImage} className="my-1 Shaun" style={{ width: "30%" }} alt="cover" />
                 </Col>
-                <Col>
-                    <h1 id="about" className="mt-3">Life and Times Of...</h1>
+            </Row>  
+            <Row> 
+                <Col className="col-md-6">
+                    <h3 id="about" className="mt-3">Life and Times Of...</h3>
                     <p>
                            I am Shaun. Avid outdoorsman, grower of beards, lover of dogs, and all else that provides peace and tranquility. Born and raised in a small town west of Ottawa, Canada, I bring that 
                        small town vibe with me wherever I go. I dream to one day own a cabin in the woods and be able to work from there, and will do whatever it takes to make that dream come true!
-
-
                     </p>
                 </Col>
-                <Col>
-                <h1 id="Shaun_Tech_Skills"> Technical Skills </h1>
-                <h1 className="ShaunLF">Languages/frameworks:</h1>
-                <p className="Shaun_Description" >- HTML, CSS, JavaScript, React,
-                    JSON, Node.js, Express.js, MySQL, Sequelize,
-                    MongoDB, Mongoose and GraphQL.
-                </p>
-                <h1 className="ShaunLF">Other Technologies:</h1>
-                <p className="Shaun_Description">- GitHub, Heroku and Visual Studio Code.</p>
-            </Col>
+                <Col className="col-md-6">
+                  <h3 id="Shaun_Tech_Skills"> Technical Skills </h3>
+                  <h5 className="ShaunLF">Languages/frameworks:</h5>
+                  <p className="Shaun_Description" >- HTML, CSS, JavaScript, React,
+                    JSON, Node.js, Express.js, MySQL, Sequelize, MongoDB, Mongoose and GraphQL.
+                  </p>
+                  <h5 className="ShaunLF">Other Technologies:</h5>
+                  <p className="Shaun_Description">- GitHub, Git, CLI, Heroku and Visual Studio Code.</p>
+                </Col>
             </Row>
+            
         </section>
         </Container>
         );
