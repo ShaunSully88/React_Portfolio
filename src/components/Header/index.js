2230d7404033101e44c51d21fb2d1ab9bc9d70c1
Shaun Sullivan <shaunsully88@gmail.com>
2022-11-17 14:07:04 -0500

    homepage not working correctly

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import pdf from '../../Resume2022.pdf';
+import pdf from '../../Shaun Sullivan Resume.pdf';
 
 function Header() {
     return (
