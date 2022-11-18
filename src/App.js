b618ecea604142314257b7a9239ea771c00d22fc
Shaun Sullivan <shaunsully88@gmail.com>
2022-11-18 14:12:00 -0500

    endpoints changed again

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           <div className="container">
             <Routes>
               <Route 
-                path="/About" 
+                path="/" 
                 element={<About />} 
               />
               <Route 
